2eb2a7fc2041c00621f92e153c0e5b97c5bdc44f
Gabriel <gabrielhepper403@gmail.com>
2023-10-27 21:43:48 -0300

    Dialog do iniciar

---
+++ arquivos/index.js
@@ -0,0 +1,7 @@
+function openDialog() {
+    document.getElementById("customDialog").style.display = "block";
+  }
+
+  function closeDialog() {
+    document.getElementById("customDialog").style.display = "none";
+  }
