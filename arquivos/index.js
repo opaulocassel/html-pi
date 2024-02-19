4c4d24a4e36991e8af006b2ca5eb35b86aa7c698
Gabriel <gabrielhepper403@gmail.com>
2024-02-19 20:41:28 -0300

    Login Pronto

--- arquivos/index.js
+++ arquivos/index.js
@@ -10,8 +10,8 @@ document.addEventListener("DOMContentLoaded", function () {
   iframe.src = `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.783434388408!2d-51.1855521!3d-29.6533521!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9519f6d3e1e86b5d%3A0x6e88d38f6e1e1e0!2sS%C3%A3o%20Leopoldo%2C%20RS%2C%20Brazil!5e0!3m2!1sen!2suk!4v1672580595752!5m2!1sen!2suk&ll=${coordenadas}&z=${zoomNivel}`;
 
   // Seleciona o elemento main da página e adiciona o iframe a ele
-  var main = document.querySelector("main");
-  main.appendChild(iframe);
+  var body = document.querySelector("body");
+  body.appendChild(iframe);
 
   // document.addEventListener("click", function (event) {
   //   var saoLeopoldoArea = document.querySelector(".iframe");
@@ -22,7 +22,9 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function openDialog() {
-  let customDialog = document.getElementById("customDialog");
+  let customDialog = document.getElementById("loginForm");
+  let form = document.getElementById("form");
+  form.style.display = "block";
   customDialog.style.display = "block";
 
   // document.addEventListener("click", function (event) {
@@ -42,6 +44,7 @@ function cadastrar(){
 }
 
 function closeDialog() {
-  document.getElementById("customDialog").style.display = "none";
-  document.getElementById("registerDialog").style.display = "none";
+  document.getElementById("form").style.display = "none";
+  document.getElementById("loginForm").style.display = "none";
+  document.getElementById("registerForm").style.display = "none";
 }
