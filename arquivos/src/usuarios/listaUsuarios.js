95588f7185038df34fbd8c6ce8df381e9d774636
Gabriel <gabrielhepper403@gmail.com>
2024-03-20 13:39:04 -0300

    Mostrando os usuários

---
+++ arquivos/src/usuarios/listaUsuarios.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("http://localhost:3000/usuarios")
+    .then((response) => response.json())
+    .then((data) => {
+      const userList = document.getElementById("userList");
+      data.forEach((usuarios) => {
+        const listaItem = document.createElement("li");
+        listaItem.textContent = `Nome: ${usuarios.nomeUsuario}, Email: ${usuarios.email}`;
+        userList.appendChild(listaItem);
+      });
+    })
+    .catch((error) =>
+      console.error("Ocorreu um erro ao carregar o arquivo JSON:", error)
+    );
+});
--- arquivos/src/usuarios/usuarios.js
+++ arquivos/src/usuarios/usuarios.js
@@ -205,17 +205,3 @@ loginForm.addEventListener('submit', (e)=>{
     }
   })
 })
-
-document.addEventListener("DOMContentLoaded", function() {
-  fetch('http://localhost:3000/usuarios')
-      .then(response => response.json())
-      .then(data => {
-          const userList = document.getElementById('userList');
-          data.forEach(usuarios => {
-              const listaItem = document.createElement('li');
-              listaItem.textContent = `Nome: ${usuarios.nome}, Email: ${usuarios.email}`;
-              userList.appendChild(listaItem);
-          });
-      })
-      .catch(error => console.error('Ocorreu um erro ao carregar o arquivo JSON:', error));
-});
