9a285d3b032896faeda4fb0e19f8e5ce694344a2
Gabriel <gabrielhepper403@gmail.com>
2024-04-09 19:02:14 -0300

    PretoBranco

--- arquivos/src/usuarios/usuarios.js
+++ arquivos/src/usuarios/usuarios.js
@@ -1,159 +1,163 @@
 async function initMap() {
-  let opcoesMapa = {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  console.log("Inicio da função.");
+
+  map = new Map(document.getElementById("map"), {
     center: { lat: -29.754732, lng: -51.151758 },
     zoom: 16,
     mapTypeId: "roadmap",
-  };
-
-  let mapElement = document.getElementById("map");
-  let map = new google.maps.Map(mapElement, opcoesMapa);
-
-  const markerShell = new google.maps.Marker({
-    map: map,
-    position: { lat: -29.754732, lng: -51.151758 },
-    id: "shell",
+    mapTypeControl: false,
   });
 
-  const selectElement = document.getElementById("selecao");
-  let selectedValue = selectElement.value;
+  console.log("Depois do map.");
 
-  if (selectedValue === "default") {
-    opcoesMapa.styles = [
-      {
-        featureType: "poi.business",
-        stylers: [{ visibility: "off" }],
-      },
-      {
-        elementType: "labels.icon",
-        stylers: [{ visibility: "off" }],
-      },
-    ];
-    map.setOptions(opcoesMapa);
-  } else if (selectedValue === "night") {
-    opcoesMapa.styles = [
-      { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
-      {
-        elementType: "labels.text.stroke",
-        stylers: [{ color: "#242f3e" }],
-      },
-      { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
-      {
-        featureType: "administrative.locality",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#d59563" }],
-      },
-      {
-        featureType: "poi",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#d59563" }],
-      },
-      {
-        featureType: "poi.park",
-        elementType: "geometry",
-        stylers: [{ color: "#263c3f" }],
-      },
-      {
-        featureType: "poi.park",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#6b9a76" }],
-      },
-      {
-        featureType: "road",
-        elementType: "geometry",
-        stylers: [{ color: "#38414e" }],
-      },
-      {
-        featureType: "road",
-        elementType: "geometry.stroke",
-        stylers: [{ color: "#212a37" }],
-      },
-      {
-        featureType: "road",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#9ca5b3" }],
-      },
-      {
-        featureType: "road.highway",
-        elementType: "geometry",
-        stylers: [{ color: "#746855" }],
-      },
-      {
-        featureType: "road.highway",
-        elementType: "geometry.stroke",
-        stylers: [{ color: "#1f2835" }],
-      },
-      {
-        featureType: "road.highway",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#f3d19c" }],
-      },
-      {
-        featureType: "transit",
-        elementType: "geometry",
-        stylers: [{ color: "#2f3948" }],
-      },
-      {
-        featureType: "transit.station",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#d59563" }],
-      },
-      {
-        featureType: "water",
-        elementType: "geometry",
-        stylers: [{ color: "#17263c" }],
-      },
-      {
-        featureType: "water",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#515c6d" }],
-      },
-      {
-        featureType: "water",
-        elementType: "labels.text.stroke",
-        stylers: [{ color: "#17263c" }],
-      },
-      {
-        featureType: "poi.business",
-        stylers: [{ visibility: "off" }],
-      },
-      {
-        elementType: "labels.icon",
-        stylers: [{ visibility: "off" }],
-      },
-    ];
-    map.setOptions(opcoesMapa);
-  }
+  const styleControl = document.getElementById("controleSelecao");
+  map.controls.push(styleControl);
 
-  document.addEventListener("DOMContentLoaded", function () {
-    document
-      .getElementById("formAtualizarNome")
-      .addEventListener("submit", function (event) {
-        event.preventDefault();
-        atualizarNomeUsuario();
-      });
+  const styleSelector = document.getElementById("selecao");
 
-    document
-      .getElementById("formAtualizarEmail")
-      .addEventListener("submit", function (event) {
-        event.preventDefault();
-        atualizarEmailUsuario();
-      });
-
-    document
-      .getElementById("formAtualizarTelefone")
-      .addEventListener("submit", function (event) {
-        event.preventDefault();
-        atualizarCelularUsuario();
-      });
-
-    document
-      .getElementById("formAtualizarSenha")
-      .addEventListener("submit", function (event) {
-        event.preventDefault();
-        atualizarSenhaUsuario();
-      });
+  map.setOptions({ styles: styles[styleSelector.value] });
+  styleSelector.addEventListener("change", () => {
+    map.setOptions({ styles: styles[styleSelector.value] });
   });
+
+  console.log("Eu funciono, mas não apareço.");
 }
+
+const styles = {
+  default: [
+    {
+      featureType: "poi.business",
+      stylers: [{ visibility: "off" }],
+    },
+    {
+      elementType: "labels.icon",
+      stylers: [{ visibility: "off" }],
+    },
+  ],
+  night: [
+    { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+    {
+      elementType: "labels.text.stroke",
+      stylers: [{ color: "#242f3e" }],
+    },
+    { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+    {
+      featureType: "administrative.locality",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#d59563" }],
+    },
+    {
+      featureType: "poi",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#d59563" }],
+    },
+    {
+      featureType: "poi.park",
+      elementType: "geometry",
+      stylers: [{ color: "#263c3f" }],
+    },
+    {
+      featureType: "poi.park",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#6b9a76" }],
+    },
+    {
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [{ color: "#38414e" }],
+    },
+    {
+      featureType: "road",
+      elementType: "geometry.stroke",
+      stylers: [{ color: "#212a37" }],
+    },
+    {
+      featureType: "road",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#9ca5b3" }],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "geometry",
+      stylers: [{ color: "#746855" }],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "geometry.stroke",
+      stylers: [{ color: "#1f2835" }],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#f3d19c" }],
+    },
+    {
+      featureType: "transit",
+      elementType: "geometry",
+      stylers: [{ color: "#2f3948" }],
+    },
+    {
+      featureType: "transit.station",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#d59563" }],
+    },
+    {
+      featureType: "water",
+      elementType: "geometry",
+      stylers: [{ color: "#17263c" }],
+    },
+    {
+      featureType: "water",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#515c6d" }],
+    },
+    {
+      featureType: "water",
+      elementType: "labels.text.stroke",
+      stylers: [{ color: "#17263c" }],
+    },
+    {
+      featureType: "poi.business",
+      stylers: [{ visibility: "off" }],
+    },
+    {
+      elementType: "labels.icon",
+      stylers: [{ visibility: "off" }],
+    },
+  ],
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("formAtualizarNome")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      atualizarNomeUsuario();
+    });
+
+  document
+    .getElementById("formAtualizarEmail")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      atualizarEmailUsuario();
+    });
+
+  document
+    .getElementById("formAtualizarTelefone")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      atualizarCelularUsuario();
+    });
+
+  document
+    .getElementById("formAtualizarSenha")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      atualizarSenhaUsuario();
+    });
+});
 
 function modoCor() {}
 
