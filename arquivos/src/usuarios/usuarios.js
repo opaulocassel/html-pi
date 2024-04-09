569815cb74f88a64b4f9eb5d795f1bea966daac1
João Vitor Silva dos Santos <jvsantos2003@hotmail.com>
2024-04-09 11:34:06 -0300

    perfil

--- arquivos/src/usuarios/usuarios.js
+++ arquivos/src/usuarios/usuarios.js
@@ -379,6 +379,8 @@ function carregarListaUsuarios() {
                     <div class="card">
                         <div class="card-body">
                             <div class="d-flex flex-column align-items-center text-center">
+                            <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin"
+                                    class="rounded-circle" width="150">                    
                                 <div class="mt-3">
                                     <h4>${usuario.nomeUsuario}</h4>
                                     <p class="text-secondary mb-1">fffffffff</p>
